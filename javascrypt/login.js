abb71654beb95df7fdd9c23b9d6f17e54b2b603b
Facu <facuu201202@gmail.com>
2023-12-05 14:17:38 -0300

    tercer commit

--- javascrypt/login.js
+++ javascrypt/login.js
@@ -9,3 +9,40 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+
+// crea un objeto para el login default
+
+let defaultUser = {
+    user: 'user@gmail.com',
+    pass: '12345'
+}
+
+
+function login(){
+    let usuario = document.getElementById('email').value;
+    let password = document.getElementById('password').value
+    console.log(`El usuario es ${usuario} y la contraseña es ${password}`)
+
+    if(usuario == "" && password == ""){
+        alert("ingresar usuario y contraseña")
+    }
+
+    if(defaultUser.user == usuario && defaultUser.pass == password){
+        location.href = "homepage.html"
+
+    }else{
+        alert("usuario o contraseña incorrecta")
+    }
+
+    let inputUser = document.getElementById('email').value;
+    inputUser = "";
+
+    let inputPass = document.getElementById('password').value;
+    inputPass = "";
+}
+
+
+
+
+
