eec73b5f2ed0e33b174f96f3cf525ad68f5eb28e
Facu <facuu201202@gmail.com>
2023-12-07 15:24:19 -0300

    quinto commit

--- javascrypt/login.js
+++ javascrypt/login.js
@@ -11,6 +11,38 @@ loginBtn.addEventListener('click', () => {
 });
 
 
+// lista de usuarios y funcion para traer los valores de los imput
+let userList = [];
+
+const data = () => {
+    nameRegister = document.getElementById("nameRegister").value;
+    emailRegister = document.getElementById("emailRegister").value;
+    passwordRegister = document.getElementById("passwordRegister").value;
+    formRegister = document.getElementById("formRegister");
+}
+
+//funcion para guarda en local storage el nuevo usuario
+
+const register = () => {
+
+    data();
+
+    let user = {
+        nameRegister,
+        emailRegister,
+        passwordRegister
+    }
+
+    userList.push(user);
+
+    localStorage.setItem("Usuarios", JSON.stringify(userList));  
+
+    console.log(userList);
+    formRegister.reset();
+}
+
+
+
 // crea un objeto para el login default
 
 let defaultUser = {
@@ -19,20 +51,45 @@ let defaultUser = {
 }
 
 
-function login(){
+//validar el login con usuario default o nuevo usuario de local storage
+
+function login() {
+
+    //datos del local storage
+
+    let usuarios = localStorage.getItem("Usuarios");
+    usuarios = JSON.parse(usuarios);
+
+
+    //datos del imput login
+
     let usuario = document.getElementById('email').value;
     let password = document.getElementById('password').value
+
     console.log(`El usuario es ${usuario} y la contraseña es ${password}`)
 
-    if(usuario == "" && password == ""){
+    let error;
+
+    //validacion del login
+
+    if (usuario == "" && password == "") {
         alert("ingresar usuario y contraseña")
     }
+    
+    for(i = 0; i <usuarios.length; i++){
 
-    if(defaultUser.user == usuario && defaultUser.pass == password){
-        location.href = "homepage.html"
+        if(defaultUser.user == usuario && defaultUser.pass == password) {
+            location.href = "homepage.html"
+            error = true;
+        }
+        else if(usuarios[i].emailRegister == usuario && usuarios[i].passwordRegister == password){
+            location.href = "homepage.html"
+            error = true;
+        }
+    }
 
-    }else{
-        alert("usuario o contraseña incorrecta")
+    if(error != true){
+        alert("usuario o contraseña incorrecta");
     }
 
     let inputUser = document.getElementById('email').value;
@@ -41,8 +98,3 @@ function login(){
     let inputPass = document.getElementById('password').value;
     inputPass = "";
 }
-
-
-
-
-
